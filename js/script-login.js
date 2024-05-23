94c0c03a44435f2472ae9c0eb17b2a9e444aaceb
difurigo <furigo9diego@gmail.com>
2024-05-22 22:27:52 -0300

    Arrumado login

--- js/script-login.js
+++ js/script-login.js
@@ -2,8 +2,8 @@ if(!localStorage.getItem("base-dados")){
 
     let listaUsuarios = [
       {txtNm:"Diego Furigo",txtCpf:"18384381924", txtEmail:"diego@email.com",txtSenha:"112233"},
-      {txtNm:"Luis Henrique",txtCpf:"93640573649", txtEmail:"luis@email.com",txtSenha:"445566"},
       {txtNm:"Melissa Pereira",txtCpf:"16395736522", txtEmail:"melissa@email.com",txtSenha:"778899"},
+      {txtNm:"Melissa Pereira",txtCpf:"16395736522", txtEmail:"luis@email.com",txtSenha:"123456"},
     ];
       localStorage.setItem("base-dados", JSON.stringify(listaUsuarios));
     } 
@@ -15,7 +15,7 @@ if(!localStorage.getItem("base-dados")){
       const elMsg = document.querySelector(".validar");
       
       for (let x = 0; x < listaUsuarios.length; x++) {
-          if((listaUsuarios[x].txtEmail == input1.value) && (listaUsuarios[x].txtSenha == input2.value)){
+          
           if((listaUsuarios[x].txtEmail == input1.value) && (listaUsuarios[x].txtSenha == input2.value)){
     
               localStorage.setItem("usuario-logado", JSON.stringify(listaUsuarios[x]));
@@ -28,20 +28,19 @@ if(!localStorage.getItem("base-dados")){
              elMsg.innerText = "Login realizado com sucesso!";
     
              setTimeout(()=>{
-                  elMsg.setAttribute("class","valida");
+                  elMsg.setAttribute("class","validar");
                   elMsg.innerText = "";
                   window.location.href = "../index.html";
              },5000);
               return false; 
           }
-      }
     }
     
       elMsg.setAttribute("class","erro");
       elMsg.innerText = "Nome de usuário ou senha incorretos!";
      
       setTimeout(()=>{
-        elMsg.setAttribute("class","valida");
+        elMsg.setAttribute("class","validar");
         elMsg.innerText = "";
       },5000);
        return false; 
